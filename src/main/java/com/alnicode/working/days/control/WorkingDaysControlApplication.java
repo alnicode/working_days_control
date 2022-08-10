835e59c7b5e114ebe99b3ecc0892eedf2e843cd5
Alben Bustamante <albenbustamante@gmail.com>
2022-08-10 11:42:32 -0500

    Initial commit

---
+++ src/main/java/com/alnicode/working/days/control/WorkingDaysControlApplication.java
@@ -0,0 +1,13 @@
+package com.alnicode.working.days.control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingDaysControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkingDaysControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alnicode/working/days/control/WorkingDaysControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alnicode.working.days.control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkingDaysControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
