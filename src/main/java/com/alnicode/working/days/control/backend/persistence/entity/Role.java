d9e0e4e07b5d666266ab44e5b3085acfc41bef61
Alben Bustamante <albenbustamante@gmail.com>
2022-08-10 12:56:59 -0500

    Create the role entity

---
+++ src/main/java/com/alnicode/working/days/control/backend/persistence/entity/Role.java
@@ -0,0 +1,58 @@
+package com.alnicode.working.days.control.backend.persistence.entity;
+
+import com.alnicode.working.days.control.util.constants.RoleConstants;
+import com.alnicode.working.days.control.util.enums.RoleType;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * The role model entity.
+ *
+ * @author Alben Bustamante
+ * @since 1.0
+ * @version 1.0
+ */
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @Column(name = "role_id")
+    private Integer id;
+
+    @Column(nullable = false, length = RoleConstants.NAME_LENGTH)
+    private String name;
+
+    /**
+     * Set a Role with a {@link RoleType} enum.
+     *
+     * @param roleType the enum to set.
+     */
+    public Role(final RoleType roleType) {
+        this.id = roleType.getId();
+        this.name = roleType.getName();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(id, role.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
